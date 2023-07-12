b9e2883df80b5fc7dc70793bee1616ef3eeb52ab
Taehoon Kim <taehoonkim.dev@gmail.com>
2023-07-12 20:45:12 +0900

    finish

---
+++ async/promise.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const promise = new Promise((resoulve, reject) => {
+
+})
+
+promise.then((value) => {
+    console.log(value);
+})
---
+++ variable.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// let name = 'taehooki';
+// console.log(name);
+// name = 'hello';
+// console.log(name);
+
+const myname = "taehoon";
+console.log(`hi ${myname}`);
+
+myname = "taehooki";
